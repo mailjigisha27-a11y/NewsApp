c88c6cb31bb8f39fea0767b6028dc31530a5f95a
mailjigisha27-a11y <jigishamail27@gmail.com>
2026-08-16 15:32:49 +0530

    Remove unused App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import News from "./Components/News";
+import Navbar from "./components/Navbar";
+import News from "./components/news";
 
 const App = () => {
   const [mode, setMode] = useState("light");
